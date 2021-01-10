387963d324ecf52db490e88130a0874cbdda779d
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-01-10 00:09:51 -0800

    removed rodio dependency

--- src/util.rs
+++
@@ -1,9 +0,0 @@
-use std::io::BufReader;
-use std::path::Path;
-use std::fs::File;
-
-/// open a file as a buffer
-pub fn open(p: impl AsRef<Path>) -> BufReader<File>
-{
-    BufReader::new(File::open(p).unwrap())
-}
