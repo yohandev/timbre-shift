396070f9629795943d9a2394b4ce2a4deb2381f0
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-01-10 00:05:38 -0800

    play music

--- src/app.rs
+++ src/app.rs
@@ -1,44 +1,17 @@
 use framework::sound::*;
 use framework::*;
 
-use crate::beep::Beep;
-
 pub struct TimbreShift
 {
     sound: Speakers,
-
-    /// (temporary) timestamp and note to play
-    notes: Vec<(u64, Beep)>,
+    ogg: SoundFile<f32>,
 }
 
 impl App for TimbreShift
 {
     fn start(&mut self)
     {
-        // // open the audio file
-        // let src = Decoder::new(util::open("res/c_major.ogg"))
-        //     .unwrap()
-        //     .convert_samples::<f32>();
-
-        // // play the audio
-        // self.audio
-        //     .play_raw(src)
-        //     .unwrap();  
-        
-        // reverse notes to pop() them in order
-        self.notes.reverse();
-    }
-
-    fn update(&mut self, time: &Time)
-    {
-        if let Some((tick, note)) = self.notes.last()
-        {
-            if time.tick() >= *tick
-            {
-                self.sound.play(note.clone());
-                self.notes.pop();
-            }
-        }
+        self.sound.play(self.ogg.track());
     }
 }
 
@@ -46,29 +19,12 @@ impl Default for TimbreShift
 {
     fn default() -> Self
     {
-        use crate::beep::notes::*;
+        // create speakers
+        let sound = Speakers::new().unwrap();
 
-        let tempo = |i: f32| { ((10.0 * i.sqrt() + 5.0) * 7.0) as u64 };
-        let notes = CHROMATIC_SCALE
-            // zelda arpeggios:
-            .windows(7)
-            .flat_map(|hz| hz
-                .iter()
-                .enumerate()
-                .filter(|(i, _)| match i { 0 | 2 | 4 | 6 => true, _ => false }))
-                .map(|(_, hz)| hz)
-            .enumerate()
-            .map(|(i, hz)| (tempo(i as f32), Beep::note(*hz)))
-            // major chords:
-            //.windows(7)
-            //.enumerate()
-            //.map(|(i, hz)| (i as u64 * 30, Beep::chord(&[hz[0], hz[3], hz[6]])))
-            .collect::<Vec<_>>();        
+        // open the audio file
+        let ogg = SoundFile::open("res/c_major.ogg").unwrap();
 
-        Self
-        {
-            sound: Speakers::new().unwrap(),
-            notes
-        }
+        Self { sound, ogg }
     }
 }
